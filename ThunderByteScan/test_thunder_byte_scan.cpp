851ae3828323f599ddc79b9b83169b90bed28c28
Pinwhell <60289470+pinwhell@users.noreply.github.com>
2023-05-09 19:58:02 -0700

    Bigger Size

--- ThunderByteScan/test_thunder_byte_scan.cpp
+++ ThunderByteScan/test_thunder_byte_scan.cpp
@@ -14,25 +14,27 @@ auto duration_##name = std::chrono::duration_cast<std::chrono::microseconds>(end
 std::cout << #name << " took " << duration_##name << " microseconds.\n";
 
 int main() {
-    
 
-    constexpr size_t pattern_size = 4;
-    uint8_t pattern[pattern_size] = { 0xAA, 0xBB, 0xCC, 0xDD };
 
-    constexpr size_t buffer_size = 1024;
-    uint8_t buffer[buffer_size];
+    constexpr size_t pattern_size = 8;
+    uint8_t pattern[pattern_size] = { 0xAA, 0xBB, 0xCC, 0xDD, 0xDD, 0xCC, 0xBB, 0xAA };
 
-    
+    char * buffer = (char*)malloc(1000000);
 
     // Insert pattern in the middle
-    const size_t pattern_offset = buffer_size / 2 - pattern_size / 2;
+    const size_t pattern_offset = 1000000 / 2 - pattern_size / 2;
     std::memcpy(&buffer[pattern_offset], pattern, pattern_size);
 
     // Perform search on buffer
-    uintptr_t result;
-    MEASURE(LocalFindPatternFirst, ThunderByteScan::LocalFindPatternFirst("AA BB CC DD", (uintptr_t)buffer, (uintptr_t)buffer + buffer_size, result));
+    ThunderByteScan::BATCHPATTERNSSCANRESULTSFIRST bpsrf;
 
-    if ((uintptr_t)buffer + pattern_offset == result)
+    uintptr_t result;
+    MEASURE(LocalFindPatternFirst, ThunderByteScan::LocalFindPatternBatchFirst({
+        "AA BB CC DD",
+        "DD CC BB AA"
+        }, (uintptr_t)buffer, (uintptr_t)buffer + 1000000, bpsrf));
+
+    if ((uintptr_t)buffer + pattern_offset == bpsrf["AA BB CC DD"])
         std::cout << "Worked";
 
     return 0;
