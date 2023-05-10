01327d30697a24a140af7264d78a46979e4fad12
Pinwhell <60289470+pinwhell@users.noreply.github.com>
2023-05-09 19:26:19 -0700

    Thread Pool Header Implemented

---
+++ ThreadPool.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <queue>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+
+class ThreadPool {
+public:
+    ThreadPool(size_t threads = std::thread::hardware_concurrency()) : stop(false) {
+        for (size_t i = 0; i < threads; ++i)
+            workers.emplace_back(
+                [this] {
+                    for (;;) {
+                        std::function<void()> task;
+                        {
+                            std::unique_lock<std::mutex> lock(this->queue_mutex);
+                            this->condition.wait(lock, [this] { return this->stop || !this->tasks.empty(); });
+                            if (this->stop && this->tasks.empty())
+                                return;
+                            task = std::move(this->tasks.front());
+                            this->tasks.pop();
+                        }
+                        task();
+                    }
+                }
+        );
+    }
+
+    template<class F, class... Args>
+    void enqueue(F&& f, Args&&... args) {
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            tasks.emplace(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+        }
+        condition.notify_one();
+    }
+
+    ~ThreadPool() {
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            stop = true;
+        }
+        condition.notify_all();
+        for (std::thread& worker : workers)
+            worker.join();
+    }
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+    std::mutex queue_mutex;
+    std::condition_variable condition;
+    bool stop;
+};
--- ThunderByteScan/test_thunder_byte_scan.cpp
+++ ThunderByteScan/test_thunder_byte_scan.cpp
@@ -1,9 +1,10 @@
 
-#define USE_SSE2
-#include <ThunderByteScan.hpp>
-#include <iostream>
-#include <fstream>
+#include <random>
+#include <algorithm>
+#include <cstring>
 #include <chrono>
+#include <iostream>
+#include <ThunderByteScan.hpp>
 
 #define MEASURE(name, x) \
 auto start_##name = std::chrono::high_resolution_clock::now(); \
@@ -13,22 +14,26 @@ auto duration_##name = std::chrono::duration_cast<std::chrono::microseconds>(end
 std::cout << #name << " took " << duration_##name << " microseconds.\n";
 
 int main() {
-    std::ifstream infile("ThunderByteScan.hpp", std::ios::binary);
-
-    // Check if file was opened successfully
-    if (!infile)
-    {
-        std::cout << "Failed to open file!" << std::endl;
-        return 1;
-    }
-
-    // Read file contents into vector
-    std::vector<char> data((std::istreambuf_iterator<char>(infile)),
-        std::istreambuf_iterator<char>());
-
-
-    uintptr_t result;
     
-    MEASURE(LocalFindPatternFirst, ThunderByteScan::LocalFindPatternFirst( "23 70 72 61 67 6D 61 20 6F 6E 63 65 0D 0A 0D 0A" , (uintptr_t)data.data(), (uintptr_t)data.data() + (size_t)data.size(), result));
+
+    constexpr size_t pattern_size = 4;
+    uint8_t pattern[pattern_size] = { 0xAA, 0xBB, 0xCC, 0xDD };
+
+    constexpr size_t buffer_size = 1024;
+    uint8_t buffer[buffer_size];
+
+    
+
+    // Insert pattern in the middle
+    const size_t pattern_offset = buffer_size / 2 - pattern_size / 2;
+    std::memcpy(&buffer[pattern_offset], pattern, pattern_size);
+
+    // Perform search on buffer
+    uintptr_t result;
+    MEASURE(LocalFindPatternFirst, ThunderByteScan::LocalFindPatternFirst("AA BB CC DD", (uintptr_t)buffer, (uintptr_t)buffer + buffer_size, result));
+
+    if ((uintptr_t)buffer + pattern_offset == result)
+        std::cout << "Worked";
+
     return 0;
 }
